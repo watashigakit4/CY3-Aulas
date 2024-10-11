d0306db17c88669c48aca4eac5ea27c5923cf313
fabiogyn157 <juninhometin2rox@gmail.com>
2024-10-11 17:34:10 -0300

    Aula 1

---
+++ aula-1/html.js
@@ -0,0 +1,24 @@
+// variavei cont e lest
+// const vem de consytante, não tem como alterar a varíavel
+// let é possível alterar a variavel 
+
+//const nome = "João"
+//let idade = 19
+
+//idade = 20
+
+//console.log(idade)
+
+// tipos de dados 
+/* string, number e Boolean - primitivos
+string - textos
+number - numeros
+boolean - verdadeiro ou falso (true or false)
+*/
+
+const nome = "olaf"
+const idade = 19
+const altura = 1.80
+const guilhermeVeio = true
+
+console.log("nome, idade, altura, guilhermVeio")
